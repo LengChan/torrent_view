ee7616eed2a49379fc9465339397603aaf7d5400
左刚华 <zuoganghua@soyoung.com>
2024-04-16 23:46:32 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ module TorrentView
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = 'Beijing'
-    config.active_record.default_timezone = :local
 
     config.active_job.queue_adapter = :sidekiq
   end
