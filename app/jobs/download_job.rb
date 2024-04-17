240bb2e32a5a27d41ec39d545c6b093a3f74019d
左刚华 <zuoganghua@soyoung.com>
2024-04-17 14:35:01 +0800

    Procfile

--- app/jobs/download_job.rb
+++ app/jobs/download_job.rb
@@ -3,9 +3,9 @@ class DownloadJob < ApplicationJob
 
   def perform(magnet)
     magnet = "magnet:?xt=urn:btih:" + magnet.gsub("magnet:?xt=urn:btih:", "")
-    Dir.chdir("#{Rails.root}/public") do
+    Dir.chdir("#{Rails.root}/public/download") do
       # 执行ls命令
-      system("torrent download #{magnet}")
+      system("/home/ubuntu/go/bin/torrent download #{magnet}")
     end
   end
 end
